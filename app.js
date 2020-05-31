1270bb5e1b9aa209756388cd2cc256c615469840
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-05-31 16:23:37 +0900

    [finish]6-2

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-alert('Hello World');
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+	num = num + i;
+}
+alert('1から10まで足し算した結果は' + num + 'です');
+
+// var max = 1000;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+// 	num = num * 2;
+// 	count = count + 1;
+// }
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+
+
+
+
+// var hello = 'Hello World';
+// alert(hello);
+
+// var int1 = 1;
+// var int2 = -10;
+// var float1 = 3.14;
+// var str1 = 'JavaScriptを覚えよう';
+
+
+// alert(4 + 3);
+// alert(8 - 5);
+// alert(2 * 6);
+// alert(10 / 2);
+
+
+
+// alert('Hello' + 'World');
+
+
+
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+
+
+
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+// 	alert('みかんの値段がりんごより安い')
+// }
+// else if (orange == apple) {
+// 	alert('みかんとりんごが同じ値段')
+// }
+// else{
+// 	alert('みかんの値段がりんごより高い')
+// }
