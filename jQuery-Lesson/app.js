bceabf80403e5e4d7ca9a1e3fe6dba386739bec1
tkamita <63448316+tkamita@users.noreply.github.com>
2020-05-31 19:11:02 +0900

    Add files via upload

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,4 +1,37 @@
+// $(function(){
+//     $('.box1').css({
+//         'background-color': '#0000FF',
+//         'height': '100px'
+//     });
+// });
 
-$(document).ready(function () {
-	$('body').html('<h1>Hello jQuery!!</h1>')
+
+// $(function(){
+//   $('.box1').slideDown();
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//     $('.box1').show();
+//     $('.box1').css({'background-color': '#0000FF'});
+// });
+
+
+// $(function(){
+// 	$('.box1').hide();
+// });
+$(function(){
+    $('.box1').slideDown(function() {
+        $('.box1').css({
+            'background-color': '#0000FF',
+            'width': '200px',
+            'height': '100px'
+        }).slideUp(5000);
+    });
+});
+$(function() {
+ 
+  // 一旦hide()で隠してフェードインさせる
+  $('.box1').show(3000);
+ 
 });
