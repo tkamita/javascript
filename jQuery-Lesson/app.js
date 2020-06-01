c0f2b58ec78457ccb7d318c24feb04ac5dc728f4
tkamita <63448316+tkamita@users.noreply.github.com>
2020-06-01 20:33:10 +0900

    [finish]6-6

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -4,8 +4,24 @@
 //         'height': '100px'
 //     });
 // });
-
-
+// $(function(){
+//     $('.box1').mouseover(function(){
+//         $('.box1').addClass('box1-ext');
+//     });
+//     $('.box1').mouseout(function(){
+//     	$('.box1').removeClass('box1-ext');
+//     });
+// });
+$(function(){
+    $('.box1').on('click', function(){
+        $('.box1').addClass('box1-ext');
+    });
+ 
+    $('.box1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
+    });
+ 
+});
 // $(function(){
 //   $('.box1').slideDown();
 //   $('.box1').slideUp();
@@ -19,19 +35,19 @@
 
 // $(function(){
 // 	$('.box1').hide();
+// // });
+// $(function(){
+//     $('.box1').slideDown(function() {
+//         $('.box1').css({
+//             'background-color': '#0000FF',
+//             'width': '200px',
+//             'height': '100px'
+//         }).slideUp(5000);
+//     });
 // });
-$(function(){
-    $('.box1').slideDown(function() {
-        $('.box1').css({
-            'background-color': '#0000FF',
-            'width': '200px',
-            'height': '100px'
-        }).slideUp(5000);
-    });
-});
-$(function() {
+// $(function() {
  
-  // 一旦hide()で隠してフェードインさせる
-  $('.box1').show(3000);
+//   // 一旦hide()で隠してフェードインさせる
+//   $('.box1').show(3000);
  
-});
+// });
