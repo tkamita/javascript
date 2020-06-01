0b9063f8c944ea5de653a723b1d9766e03f2a95a
tkamita <63448316+tkamita@users.noreply.github.com>
2020-06-01 20:46:18 +0900

    [finish]6-7

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,53 +1,5 @@
-// $(function(){
-//     $('.box1').css({
-//         'background-color': '#0000FF',
-//         'height': '100px'
-//     });
-// });
-// $(function(){
-//     $('.box1').mouseover(function(){
-//         $('.box1').addClass('box1-ext');
-//     });
-//     $('.box1').mouseout(function(){
-//     	$('.box1').removeClass('box1-ext');
-//     });
-// });
 $(function(){
-    $('.box1').on('click', function(){
-        $('.box1').addClass('box1-ext');
+    $('button').on('click', function(){
+        $('ul').children().css('color','red');
     });
- 
-    $('.box1').mouseout(function(){
-        $('.box1').removeClass('box1-ext');
-    });
- 
 });
-// $(function(){
-//   $('.box1').slideDown();
-//   $('.box1').slideUp();
-// });
-
-// $(function(){
-//     $('.box1').show();
-//     $('.box1').css({'background-color': '#0000FF'});
-// });
-
-
-// $(function(){
-// 	$('.box1').hide();
-// // });
-// $(function(){
-//     $('.box1').slideDown(function() {
-//         $('.box1').css({
-//             'background-color': '#0000FF',
-//             'width': '200px',
-//             'height': '100px'
-//         }).slideUp(5000);
-//     });
-// });
-// $(function() {
- 
-//   // 一旦hide()で隠してフェードインさせる
-//   $('.box1').show(3000);
- 
-// });
